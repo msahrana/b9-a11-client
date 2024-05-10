84ba0b99854d8bc22066beabfd5c27e9290b1d21
msahrana <msahrana@gmail.com>
2024-05-10 14:49:38 +0600

    footer part is add

--- src/layout/Root/Root.jsx
+++ src/layout/Root/Root.jsx
@@ -1,9 +1,17 @@
 import {Outlet} from "react-router-dom";
+// import Navbar from "../../shared/Navbar/Navbar";
+import Footer from "../../shared/Footer/Footer";
 
 const Root = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <div className="container max-w-screen-2xl mx-auto">
+        {/* <Navbar></Navbar> */}
+        <div>
+          <Outlet></Outlet>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
