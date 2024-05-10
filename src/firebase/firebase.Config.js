ea877292393e61f26bb476b2cfa442ccb0f48960
msahrana <msahrana@gmail.com>
2024-05-10 15:07:51 +0600

    firebase config file added

---
+++ src/firebase/firebase.Config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmHsybwZuieGo63dMsDrAVGe-FT7cn6XU",
+  authDomain: "job-nest-8cab6.firebaseapp.com",
+  projectId: "job-nest-8cab6",
+  storageBucket: "job-nest-8cab6.appspot.com",
+  messagingSenderId: "973555998879",
+  appId: "1:973555998879:web:b1acee9a50a26c2df702aa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/layout/Root/Root.jsx
+++ src/layout/Root/Root.jsx
@@ -1,12 +1,12 @@
 import {Outlet} from "react-router-dom";
-// import Navbar from "../../shared/Navbar/Navbar";
+import Navbar from "../../shared/Navbar/Navbar";
 import Footer from "../../shared/Footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <div className="container max-w-screen-2xl mx-auto">
-        {/* <Navbar></Navbar> */}
+        <Navbar></Navbar>
         <div>
           <Outlet></Outlet>
         </div>
